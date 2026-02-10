b1e27149f50029b7799cdf693252dd3b48984f80
Sujal-02 <sujalkaware921@gmail.com>
2026-02-10 23:42:08 +0530

    fixes

--- .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_globals_71f961d1.css",
-  "static/chunks/node_modules_5aaf8ec1._.js",
-  "static/chunks/_47f3a1b6._.js"
-],
-    source: "dynamic"
-});
